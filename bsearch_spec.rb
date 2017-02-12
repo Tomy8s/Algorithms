1a10d680c60c99a9c6595d2814e5598aededcbcd
Tomy8s <tomy8s@hotmail.com>
2017-02-12 22:32:18 +0000

    test for high target error

--- bsearch_spec.rb
+++ bsearch_spec.rb
@@ -2,6 +2,8 @@ require_relative 'bsearch.rb'
 
 describe Bsearch do
   let(:subject) { Bsearch.new(5, 0, 100) }
+  let(:subject_hi) { Bsearch.new(105, 0, 100) }
+  let(:subject_lo) { Bsearch.new(-15, 0, 100) }
   describe '#initialize' do
     it 'correctly assigns the target' do
       expect(subject.instance_variable_get(:@target)).to eq  5
@@ -13,6 +15,12 @@ describe Bsearch do
 
     it 'correctly assigns the end of the array' do
       expect(subject.instance_variable_get(:@arr)[-1]).to eq  100
+    end
+  end
+
+  describe '#check' do
+    it 'throws an error if target greater than array values' do
+      expect { subject_hi.search }.to raise_error ArgumentError
     end
   end
 end
