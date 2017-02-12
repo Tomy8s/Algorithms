c2739a5e6de5871ae29767f2f1b5e6e7615a0dd5
Tomy8s <tomy8s@hotmail.com>
2017-02-12 21:34:57 +0000

    tests for '#initialize'

--- bsearch_spec.rb
+++ bsearch_spec.rb
@@ -1,9 +1,18 @@
 require_relative 'bsearch.rb'
 
 describe Bsearch do
+  let(:subject) { Bsearch.new(5, 0, 100) }
   describe '#initialize' do
     it 'correctly assigns the target' do
-      expect(Bsearch.new(5, 0, 100).target).to eq  5
+      expect(subject.instance_variable_get(:@target)).to eq  5
+    end
+
+    it 'correctly assigns the start of the array' do
+      expect(subject.instance_variable_get(:@arr)[0]).to eq  0
+    end
+
+    it 'correctly assigns the end of the array' do
+      expect(subject.instance_variable_get(:@arr)[-1]).to eq  100
     end
   end
 end
