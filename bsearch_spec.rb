912c53cd3ffc026e515f7b9eadd428fbc1886cab
Tomy8s <tomy8s@hotmail.com>
2017-02-12 22:45:47 +0000

    test for wrong way round array arguments

--- bsearch_spec.rb
+++ bsearch_spec.rb
@@ -2,6 +2,7 @@ require_relative 'bsearch.rb'
 
 describe Bsearch do
   let(:subject) { Bsearch.new(5, 0, 100) }
+  let(:subject_inv) { Bsearch.new(5, 100, 0) }
   let(:subject_hi) { Bsearch.new(105, 0, 100) }
   let(:subject_lo) { Bsearch.new(-15, 0, 100) }
   describe '#initialize' do
@@ -13,8 +14,16 @@ describe Bsearch do
       expect(subject.instance_variable_get(:@arr)[0]).to eq  0
     end
 
+    it 'correctly assigns inverted values to the start of the array' do
+      expect(subject_inv.instance_variable_get(:@arr)[0]).to eq  0
+    end
+
     it 'correctly assigns the end of the array' do
       expect(subject.instance_variable_get(:@arr)[-1]).to eq  100
+    end
+
+    it 'correctly assigns inverted values to the end of the array' do
+      expect(subject_inv.instance_variable_get(:@arr)[-1]).to eq  100
     end
   end
 
